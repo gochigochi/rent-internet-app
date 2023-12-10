da3dc4dd5ca634e5d88525138a0a536b5f431a5b
diego <diegoeliseoiovane@gmail.com>
2023-12-10 20:00:07 -0300

    dev br

--- utils/createCheckoutSession.js
+++ utils/createCheckoutSession.js
@@ -2,8 +2,6 @@ import { loadStripe } from '@stripe/stripe-js'
 
 export const createCheckOutSession = async ( cart, customer ) => {
 
-    console.log('En createCheckoutSession cart............', cart)
-
     const publishableKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 
     const stripePromise = loadStripe(publishableKey)
@@ -12,9 +10,6 @@ export const createCheckOutSession = async ( cart, customer ) => {
         const stripe = await stripePromise
         stripe.redirectToCheckout({sessionId: id})
     }
-
-    //probar hacer el get products de woo que hay en create-stripe-session aca, y despues pasarle los
-    //productos para reducir el tiempo de la api call
 
     fetch('/api/create-stripe-session', {
         method: "POST",
